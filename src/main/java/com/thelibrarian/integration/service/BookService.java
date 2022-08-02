b8b5b246657677ff6ee524499dadbb1e7fc981d1
OPTIMISSA\ivaylo.trifonov <ivaylo.trifonov@optimissa.com>
2022-08-02 11:28:48 +0200

    ﻿#27173 Creado el servicio de busquedo por titulo y autor

--- src/main/java/com/thelibrarian/integration/service/BookService.java
+++ src/main/java/com/thelibrarian/integration/service/BookService.java
@@ -2,9 +2,8 @@ package com.thelibrarian.integration.service;
 
 import com.thelibrarian.integration.dto.BookDataDto;
 
-
 public interface BookService {
-
     public BookDataDto getBook();
+    public BookDataDto searchBookByTitleAuthor(String title, String author);
     public BookDataDto getBookByIsbn(String isbn);
 }
