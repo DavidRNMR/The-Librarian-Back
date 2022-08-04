536e4b5b6d3d9caaf50dfdf355aa55767c2cfca9
OPTIMISSA\david.rivera <david.rivera@optimissa.com>
2022-08-04 08:25:24 +0200

    #27213 soluciono errores de bookcontrollerapi

--- src/main/java/com/thelibrarian/core/controller/BookControllerApi.java
+++ src/main/java/com/thelibrarian/core/controller/BookControllerApi.java
@@ -1,6 +1,7 @@
 package com.thelibrarian.core.controller;
 
 import com.thelibrarian.integration.dto.BookDataDto;
+import com.thelibrarian.integration.service.BookServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
