cff024e102cf789d1a7eb855fd6e762c64b1a03a
OPTIMISSA\ivaylo.trifonov <ivaylo.trifonov@optimissa.com>
2022-08-05 14:12:57 +0200

    [#27237] Creados dos enums con datos para la necesidad de los tests

--- src/test/java/com/thelibrarian/TheLibrarianApplicationTests.java
+++ src/test/java/com/thelibrarian/TheLibrarianApplicationTests.java
@@ -3,64 +3,91 @@ package com.thelibrarian;
 import com.thelibrarian.integration.dto.BookDataDto;
 import com.thelibrarian.integration.dto.BookDto;
 import com.thelibrarian.integration.service.BookServiceImpl;
+import com.thelibrarian.integration.utilities.AuthorsForTest;
+import com.thelibrarian.integration.utilities.TitlesForTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class TheLibrarianApplicationTests {
 
-	private static BookDataDto randomBooks;
-	private static BookDto[] booksByAuthor;
-	private static BookServiceImpl bookService;
-	private static String author;
+    private static BookDataDto randomBooks;
+    private static BookDto[] booksArray;
+    private static BookServiceImpl bookService;
 
-	@BeforeAll
-	public static void setUp() {
-		bookService = new BookServiceImpl();
-		randomBooks = bookService.getRandomBooks().getBody();
-		author = "King";
-	}
+    @BeforeAll
+    public static void setUp() {
+        bookService = new BookServiceImpl();
+        randomBooks = bookService.getRandomBooks().getBody();
+    }
 
-	@Test
-	@DisplayName("Test size of random books list")
-	public void getBooksCount() {
-		Assertions.assertEquals(36, randomBooks.items.length);
-	}
+    @Test
+    @DisplayName("Test size of random books list")
+    public void getBooksCount() {
+        Assertions.assertEquals(36, randomBooks.items.length);
+    }
 
-	@Test()
-	@DisplayName("Test book language")
-	public void getException() {
-		Exception exception = Assertions.assertThrows(NumberFormatException.class, () -> {
-			Integer.parseInt(randomBooks.items[0].volumeInfo.getLanguage());
-		});
+    @Test()
+    @DisplayName("Test book language")
+    public void getException() {
+        Exception exception = Assertions.assertThrows(NumberFormatException.class, () -> {
+            Integer.parseInt(randomBooks.items[0].volumeInfo.getLanguage());
+        });
 
-		String expectedMessage = "For input string: \"" + randomBooks.items[0].volumeInfo.getLanguage() + "\"";
-		String actualMessage = exception.getMessage();
+        String expectedMessage = "For input string: \"" + randomBooks.items[0].volumeInfo.getLanguage() + "\"";
+        String actualMessage = exception.getMessage();
 
-		Assertions.assertEquals(expectedMessage, actualMessage);
-	}
+        Assertions.assertEquals(expectedMessage, actualMessage);
+    }
 
-	@Test
-	@DisplayName("Test book by author")
-	public void GetBookByAuthor() {
-		booksByAuthor = bookService.getBookByAuthor(author).getBody().getItems();
-		String authorActualName = "";
+    @Test
+    @DisplayName("Test book by author")
+    public void getBookByAuthor() {
 
-		for (BookDto book : booksByAuthor) {
-			if (book.getVolumeInfo().getAuthors() != null) {
-				for (int i = 0; i < book.getVolumeInfo().getAuthors().length; i++) {
-					if (book.getVolumeInfo().getAuthors()[i].contains(author)) {
-						authorActualName = book.getVolumeInfo().getAuthors()[i];
-					}
-					Assertions.assertEquals(authorActualName, book.getVolumeInfo().getAuthors()[1]);
-				}
-			}
-		}
-	}
+        String authorActualName;
+
+        for (AuthorsForTest author : AuthorsForTest.values()) {
+
+            booksArray = bookService.getBookByAuthor(author.name()).getBody().getItems();
+
+            for (BookDto book : booksArray) {
+                if (book.getVolumeInfo().getAuthors() != null) {
+                    for (int i = 0; i < book.getVolumeInfo().getAuthors().length; i++) {
+                        if (book.getVolumeInfo().getAuthors()[i].contains(author.name())) {
+                            authorActualName = book.getVolumeInfo().getAuthors()[i];
+                            Assertions.assertEquals(authorActualName, book.getVolumeInfo().getAuthors()[i]);
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+    }
+
+    @Test
+    @DisplayName("Test book by title")
+    public void getBookByTitle() {
+
+        String actualTitle;
+
+        for (TitlesForTest title : TitlesForTest.values()) {
+            booksArray = bookService.getBookByTitle(title.name()).getBody().getItems();
+
+            for (BookDto book : booksArray) {
+                if (book.getVolumeInfo().getTitle().contains(title.name())) {
+                    actualTitle = book.getVolumeInfo().getTitle();
+                    Assertions.assertEquals(actualTitle, book.getVolumeInfo().getTitle());
+                }
+
+            }
+        }
+    }
 
 }
