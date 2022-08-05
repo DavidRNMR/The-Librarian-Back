7aba9baaa04a6891b438005a9497d1cbd69bff87
OPTIMISSA\ivaylo.trifonov <ivaylo.trifonov@optimissa.com>
2022-08-05 11:32:05 +0200

    #27237 Creados los primeros tests de la funcionalidad de los metodos que hacen llamadas a la API

--- src/test/java/com/thelibrarian/TheLibrarianApplicationTests.java
+++ src/test/java/com/thelibrarian/TheLibrarianApplicationTests.java
@@ -1,13 +1,62 @@
 package com.thelibrarian;
 
+import com.thelibrarian.integration.dto.BookDataDto;
+import com.thelibrarian.integration.dto.BookDto;
+import com.thelibrarian.integration.service.BookServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class TheLibrarianApplicationTests {
 
+	private static BookDataDto randomBooks;
+	private static BookDto[] booksByAuthor;
+	private static BookServiceImpl bookService;
+	private static String author;
+
+	@BeforeAll
+	public static void setUp() {
+		bookService = new BookServiceImpl();
+		randomBooks = bookService.getRandomBooks().getBody();
+		author = "King";
+	}
+
 	@Test
-	void contextLoads() {
+	@DisplayName("Test size of random books list")
+	public void getBooksCount() {
+		Assertions.assertEquals(36, randomBooks.items.length);
+	}
+
+	@Test()
+	@DisplayName("Test book language")
+	public void getException() {
+		Exception exception = Assertions.assertThrows(NumberFormatException.class, () -> {
+			Integer.parseInt(randomBooks.items[0].volumeInfo.getLanguage());
+		});
+
+		String expectedMessage = "For input string: \"" + randomBooks.items[0].volumeInfo.getLanguage() + "\"";
+		String actualMessage = exception.getMessage();
+
+		Assertions.assertEquals(expectedMessage, actualMessage);
+	}
+
+	@Test
+	@DisplayName("Test book by author")
+	public void GetBookByAuthor() {
+		booksByAuthor = bookService.getBookByAuthor(author).getBody().getItems();
+		String authorActualName = "";
+
+		for (BookDto book : booksByAuthor) {
+			if (book.getVolumeInfo().getAuthors()[0].contains(author)) {
+				authorActualName = book.getVolumeInfo().getAuthors()[0];
+			}
+			Assertions.assertEquals(authorActualName, book.getVolumeInfo().getAuthors()[0]);
+		}
 	}
 
 }
