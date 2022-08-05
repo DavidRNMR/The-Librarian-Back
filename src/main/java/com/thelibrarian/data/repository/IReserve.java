aff22e2aca54dc0dfb1c37efd3fc1c7c84430c9c
OPTIMISSA\raul.gonzalez <raul.gonzalez@optimissa.com>
2022-08-05 14:00:49 +0200

    [#27524] Creado el servicio crear reserva con el DTO, a falta de crear un metodo mapper para utilizarlo siempre.

--- src/main/java/com/thelibrarian/data/repository/IReserve.java
+++ src/main/java/com/thelibrarian/data/repository/IReserve.java
@@ -3,12 +3,13 @@ package com.thelibrarian.data.repository;
 
 
 import org.springframework.http.ResponseEntity;
-import com.thelibrarian.data.entity.ReservationEntity;
+import com.thelibrarian.data.dto.BookingDto;
+
 
 
 public interface IReserve {
 
 
-   public ResponseEntity<ReservationEntity> createReserve(ReservationEntity booking);
+   public ResponseEntity<BookingDto> createReserve(BookingDto booking);
     
 }
