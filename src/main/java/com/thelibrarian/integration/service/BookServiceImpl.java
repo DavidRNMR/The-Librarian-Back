ce3e741a49bcb9b42a44626108319ed505269113
OPTIMISSA\ivaylo.trifonov <ivaylo.trifonov@optimissa.com>
2022-08-08 11:53:37 +0200

    ﻿[#27285] Creado metodo de comprobacion de campos con valor null a la hora de traer los datos de un libro y aplicado formateo del codigo

--- src/main/java/com/thelibrarian/integration/service/BookServiceImpl.java
+++ src/main/java/com/thelibrarian/integration/service/BookServiceImpl.java
@@ -8,14 +8,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class BookServiceImpl implements BookService {
 
-    RestTemplate restTemplate = new RestTemplate();
-
-    String url = "https://www.googleapis.com/books/v1/volumes?q=";
-
     final String APIKEY = "&key=AIzaSyD6yt9yJuDcLr8ZVAPLiKGWoBzWgQtJHN4&maxResults=36";
+    RestTemplate restTemplate = new RestTemplate();
+    String url = "https://www.googleapis.com/books/v1/volumes?q=";
 
     @Override
     public ResponseEntity<BookDataDto> getRandomBooks() {
@@ -25,11 +26,10 @@ public class BookServiceImpl implements BookService {
         BookDataDto bookDataDto = restTemplate.getForObject(urlGetBook, BookDataDto.class);
 
         if (bookDataDto == null) {
-
             return ResponseEntity.notFound().build();
         }
 
-        return ResponseEntity.ok().body(bookDataDto);
+        return ResponseEntity.ok().body(checkCorrectDataInsert(bookDataDto));
 
     }
 
@@ -50,14 +50,12 @@ public class BookServiceImpl implements BookService {
     @Override
     public ResponseEntity<BookDataDto> getBookByIsbn(String isbn) {
 
-      String urlIsbn = url + "isbn:" + isbn + APIKEY;
+        String urlIsbn = url + "isbn:" + isbn + APIKEY;
 
         BookDataDto bookDataDtoIsbn = restTemplate.getForObject(urlIsbn, BookDataDto.class);
 
         if (bookDataDtoIsbn == null) {
-
             return ResponseEntity.notFound().build();
-
         }
 
         return ResponseEntity.ok().body(bookDataDtoIsbn);
@@ -69,14 +67,9 @@ public class BookServiceImpl implements BookService {
 
         String urlAuthor = url + "+inauthor:" + Author + APIKEY;
 
-      //  BookDataDto bookDataDto = new BookDataDto();
-
-       BookDataDto bookDataDto = restTemplate.getForObject(urlAuthor, BookDataDto.class);
-
-
+        BookDataDto bookDataDto = restTemplate.getForObject(urlAuthor, BookDataDto.class);
 
         if (bookDataDto == null) {
-
             return ResponseEntity.notFound().build();
         }
 
@@ -95,6 +88,54 @@ public class BookServiceImpl implements BookService {
         }
 
         return ResponseEntity.ok().body(bookDataDto);
+    }
+
+    private BookDataDto checkCorrectDataInsert(BookDataDto bookDataDto) {
+
+        for (int i = 0; i < bookDataDto.getItems().length; i++) {
+            System.out.println(bookDataDto.getItems()[i].getVolumeInfo().getTitle());
+            if (bookDataDto.getItems()[i].getVolumeInfo().getTitle() == null) {
+                bookDataDto.getItems()[i].getVolumeInfo().setTitle("No title available.");
+            }
+
+            if (bookDataDto.getItems()[i].getVolumeInfo().getAuthors() == null) {
+                String[] authors = {"No authors info available."};
+                bookDataDto.getItems()[i].getVolumeInfo().setAuthors(authors);
+            }
+
+            if (bookDataDto.getItems()[i].getVolumeInfo().getPublishedDate() == null) {
+                bookDataDto.getItems()[i].getVolumeInfo().setPublishedDate("No published date available.");
+            }
+
+            if (bookDataDto.getItems()[i].getVolumeInfo().getDescription() == null) {
+                bookDataDto.getItems()[i].getVolumeInfo().setDescription("No description available.");
+            }
+
+            if (bookDataDto.getItems()[i].getVolumeInfo().getImageLinks() == null) {
+                Map<String, String> imageLinks = new HashMap<>();
+                imageLinks.put("smallThumbnail", "No image available.");
+                bookDataDto.getItems()[i].getVolumeInfo().setImageLinks(imageLinks);
+            }
+
+            if (bookDataDto.getItems()[i].getVolumeInfo().getCategories() == null) {
+                String[] categories = {"No categories specified."};
+                bookDataDto.getItems()[i].getVolumeInfo().setCategories(categories);
+            }
+
+            if (bookDataDto.getItems()[i].getVolumeInfo().getLanguage() == null) {
+                bookDataDto.getItems()[i].getVolumeInfo().setLanguage("Language not specified");
+            }
+
+            if (bookDataDto.getItems()[i].getVolumeInfo().getIndustryIdentifiers() == null) {
+                Map<String, String> isbn = new HashMap<>();
+                isbn.put("identifier", "No isbn available.");
+                Map<String, String>[] industryIdentifiers = new HashMap[1];
+                industryIdentifiers[0] = isbn;
+                bookDataDto.getItems()[i].getVolumeInfo().setIndustryIdentifiers(industryIdentifiers);
+            }
+        }
+
+        return bookDataDto;
     }
 
 }
