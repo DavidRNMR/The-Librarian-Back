1deea8824411339941cd375dd46adc5165d95d70
OPTIMISSA\johansel.calderon <johansel.calderon@hotmail.com>
2022-08-08 11:14:27 +0200

    [#27277] Bug interface reserva arreglado

--- src/main/java/com/thelibrarian/data/repository/IReserve.java
+++ src/main/java/com/thelibrarian/data/repository/IReserve.java
@@ -3,13 +3,14 @@ package com.thelibrarian.data.repository;
 
 
 import org.springframework.http.ResponseEntity;
-import com.thelibrarian.data.dto.BookingDto;
+
+import com.thelibrarian.data.entity.ReservationEntity;
 
 
 
 public interface IReserve {
 
 
-   public ResponseEntity<BookingDto> createReserve(BookingDto booking);
+   public ResponseEntity<ReservationEntity> createReserve(ReservationEntity booking);
     
 }
