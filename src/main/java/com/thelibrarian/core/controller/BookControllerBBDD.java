50b845ee8cd7c9ae4a6ef7a3b050421f3ed9a4f2
OPTIMISSA\david.rivera <david.rivera@optimissa.com>
2022-08-17 10:31:52 +0200

    [#27446] cambiando a 12 los random books

--- src/main/java/com/thelibrarian/core/controller/BookControllerBBDD.java
+++ src/main/java/com/thelibrarian/core/controller/BookControllerBBDD.java
@@ -8,14 +8,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.thelibrarian.data.dto.BookDto;
 import com.thelibrarian.data.entity.BookEntity;
@@ -72,15 +65,16 @@ import io.swagger.v3.oas.models.responses.ApiResponse;
         }
 
         @GetMapping("/getByIdBook/{id}")
-        public  BookDto findById(@PathVariable Integer id) {
+        @ResponseBody
+        public  ResponseEntity <BookDto> findById(@PathVariable Integer id) {
 
-            /*BookEntity book = bookService.findById(id);
+            BookEntity book = bookService.findById(id);
 
             BookDto bookDto = modelMapper.map(book,BookDto.class);
 
-            return ResponseEntity.ok().body(bookDto);*/
+            return ResponseEntity.ok().body(bookDto);
 
-            return convertToDto(bookService.findById(id));
+
         }
 
         @GetMapping("/getByIsbn/{isbn}")
@@ -94,9 +88,5 @@ import io.swagger.v3.oas.models.responses.ApiResponse;
 
         }
 
-        private BookDto convertToDto(BookEntity book) {
-            BookDto bookDto = modelMapper.map(book, BookDto.class);
-            return bookDto;
-        }
     }
 
