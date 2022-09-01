1faf67a634b5c479a5b0b7183f80fe36e622375f
OPTIMISSA\raul.gonzalez <raul.gonzalez@optimissa.com>
2022-09-01 09:42:47 +0200

    Arreglado email service comentado de David

--- src/main/java/com/thelibrarian/integration/service/EmailService.java
+++ src/main/java/com/thelibrarian/integration/service/EmailService.java
@@ -13,24 +13,23 @@ import java.io.IOException;
 @Service
 public class EmailService {
 
-//    @Autowired
-//    private JavaMailSender emailSender;
-//
-//    private ObjectMapper objectMapper = new ObjectMapper();
-//
-//    public void sendEmail(String userName, String userEmail) throws IOException {
-//        Mail mail = objectMapper.readValue(new File("src/main/resources/emailData.json"), Mail.class);
-//        mail.setTo(userEmail);
-//        mail.setContent("Hola, " + userName + "!\n\n" + mail.getContent());
-//
-//        SimpleMailMessage message = new SimpleMailMessage();
-//        message.setSubject(mail.getSubject());
-//        message.setFrom(mail.getFrom());
-//        message.setTo(mail.getTo());
-//        message.setText(mail.getContent());
-//
-//        emailSender.send(message);
-//
-//    }
+   @Autowired
+   private JavaMailSender emailSender;
+
+   private ObjectMapper objectMapper = new ObjectMapper();
+  public void sendEmail(String userName, String userEmail) throws IOException {
+      Mail mail = objectMapper.readValue(new File("src/main/resources/emailData.json"), Mail.class);
+       mail.setTo(userEmail);
+     mail.setContent("Hola, " + userName + "!\n\n" + mail.getContent());
+
+      SimpleMailMessage message = new SimpleMailMessage();
+        message.setSubject(mail.getSubject());
+       message.setFrom(mail.getFrom());
+       message.setTo(mail.getTo());
+       message.setText(mail.getContent());
+
+      emailSender.send(message);
+
+    }
 
 }
